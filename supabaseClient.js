fea34ec2bb96c9638f263e16eb28f3f1719354cb
Dorigondudu <bovo.eduardo@escola.pr.gov.br>
2025-05-13 07:50:29 -0300

    kufu

--- supabaseClient.js
+++ supabaseClient.js
@@ -4,4 +4,4 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 // Cria o client com a URL e a chave fornecidas
 export const supabase = createClient(
   'https://rrnwxsieiecmcsmtryux.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJybnd4c2llaWVjbWNzbXRyeXV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1MzU1MjIsImV4cCI6MjA2MjExMTUyMn0.F5KZm-HiVbd9W12P3figvuJl6PObzRIbQvmg4ioxdho'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJybnd4c2llaWVjbWNzbXRyeXV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1MzU1MjIsImV4cCI6MjA2MjExMTUyMn0.F5KZm-HiVbd9W12P3figvuJl6PObzRIbQvmg4ioxdho')
