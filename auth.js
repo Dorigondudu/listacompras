95d8c063fb214e4443c901fbb3a7ec8bb9455f73
Dorigondudu <bovo.eduardo@escola.pr.gov.br>
2025-05-19 12:06:41 -0300

    ddddd

--- auth.js
+++ auth.js
@@ -27,7 +27,7 @@ window.cadastro = async function () {
     alert('Cadastro realizado! Faça login.')
   }
 }
-document.addEventListener('keydown'),function (event) {
+document.addEventListener('keydown'), function (event) {
     if (event.key === 'Enter') {
       const caminho = window.location.pathname
       if (caminho.includes('login')) {
