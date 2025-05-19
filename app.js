3fb5ba4e7abe9ce7f0c67528612f6c9c7549784d
Dorigondudu <bovo.eduardo@escola.pr.gov.br>
2025-05-19 12:03:08 -0300

    abrrrrraaaaao

--- app.js
+++ app.js
@@ -63,11 +63,6 @@ getUser().then(carregarLista)
 
 document.addEventListener('keydown'),function (event) {
     if (event.key ==='Enter') {
-      const caminho = window.location.pathname
-      if (caminho.includes('login')) {
-        login() 
-      } else if (caminho.includes('cadastro')) {
-        cadastro()
+      adicionarItem()
       }
     }
-}
